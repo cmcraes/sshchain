d830e4fad49794d4ea3142ca1c795f44f29e9a8c
Philipp Jaeger <pjaeger@physik.uni-kl.de>
2016-06-25 21:45:28 -0500

    fixed documentation

--- mpcomplex.cpp
+++ mpcomplex.cpp
@@ -5,9 +5,9 @@
 #include "mpcomplex.h"
 
 /**
+* Constructor with GMP numbers
 * \param x real part
 * \param y imaginary part
-* Constructor with GMP numbers
 */
 mpcomplex::mpcomplex(const mpfloat_t x, const mpfloat_t y) {
     re = x;
@@ -15,8 +15,8 @@ mpcomplex::mpcomplex(const mpfloat_t x, const mpfloat_t y) {
 }
 
 /**
-* \param x real part
 * Constructor with one GMP number
+* \param x real part
 */
 mpcomplex::mpcomplex(const mpfloat_t x) {
     re = x;
@@ -59,6 +59,10 @@ mpcomplex mpcomplex::operator=(const mpfloat_t a) {
     return *this;
 }
 
+/**
+ * Conjugate the number
+ * \returns the complex conjugate of the number it is called on
+ */
 mpcomplex mpcomplex::cconj() {
     return mpcomplex(re, -im);
 }
@@ -66,6 +70,7 @@ mpcomplex mpcomplex::cconj() {
 /**
  * Addition operator
  * Accepts mpcomplex, and mpfloat_t (which wraps the standard types)
+ * \memberof mpcomplex
  */
 mpcomplex operator+(const mpcomplex a, const mpcomplex b) {
     return mpcomplex(b.re + a.re, b.im + a.im);
@@ -78,6 +83,7 @@ mpcomplex operator+(const mpcomplex a, const mpfloat_t b) {
 /**
  * Substraction operator
  * Accepts mpcomplex, and mpfloat_t (which wraps the standard types)
+ * \memberof mpcomplex
  */
 mpcomplex operator-(const mpcomplex a, const mpcomplex b) {
     return mpcomplex(b.re - a.re, b.im - a.im);
@@ -90,6 +96,7 @@ mpcomplex operator-(const mpcomplex a, const mpfloat_t b) {
 /**
  * Multiplication operator
  * Accepts mpcomplex, and mpfloat_t (which wraps the standard types)
+ * \memberof mpcomplex
  */
 mpcomplex operator*(const mpcomplex a, const mpcomplex b) {
     return mpcomplex((a.re * b.re - a.im * b.im), (a.re * b.im + a.im * b.re));
@@ -102,6 +109,7 @@ mpcomplex operator*(const mpcomplex a, const mpfloat_t b) {
 /**
  * Division operator
  * Accepts mpcomplex, and mpfloat_t (which wraps the standard types)
+ * \memberof mpcomplex
  */
 mpcomplex operator/(const mpcomplex a, const mpfloat_t b) {
     return mpcomplex(a.re / b, a.im / b);
